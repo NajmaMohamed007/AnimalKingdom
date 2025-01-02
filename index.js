d043d890b672c2241cd8faef5c76654ebb9078c5
najmamohamed007 <nmoha007@campus.goldsmiths.ac.uk>
2025-01-02 19:25:51 +0000

    Final

--- index.js
+++ index.js
@@ -2,28 +2,118 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
+const mysql = require('mysql2');
 
-const db = mysql.createConnection ({
+// Database connection
+const db = mysql.createConnection({
   host: 'localhost',
   user: 'portfolio_app',
   password: 'qwertyuiop',
   database: 'portfolio'
-})
+});
+
 // Connect to the database
 db.connect((err) => {
   if (err) {
-      throw err
+    throw err;
   }
-  console.log('Connected to database')
-})
-global.db = db
+  console.log('Connected to the database');
+});
 
-const mainRoutes = require('./routes/main');
-
-// Set the view engine to ejs
+// Set view engine to EJS
 app.set('view engine', 'ejs');
-app.use('/', mainRoutes);
 
+// Middleware to parse URL-encoded bodies (form data)
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve static files (CSS, JS, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the homepage (GET)
+app.get('/', (req, res, next) => {
+  const query = `SELECT Animals.name AS animal_name, Animals.species, Continents.continent_name 
+                 FROM Animals
+                 JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
+                 JOIN Continents ON Animal_Continents.continent_id = Continents.id`;
+
+  db.query(query, (err, animals) => {
+    if (err) {
+      return next(err);
+    }
+    res.render('index', { animals });
+  });
+});
+
+// Route for adding a new animal (POST)
+app.post('/', (req, res, next) => {
+  const { animal_name, species, continent } = req.body;
+
+  // Ensure required fields are provided
+  if (!animal_name || !species || !continent) {
+    return res.status(400).send("Missing required fields.");
+  }
+
+  // Insert the new animal into the Animals table
+  const animalQuery = `INSERT INTO Animals (name, species) VALUES (?, ?)`;
+  db.query(animalQuery, [animal_name, species], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+
+    // After inserting the animal, find the corresponding continent
+    const getContinentQuery = `SELECT id FROM Continents WHERE continent_name = ?`;
+    db.query(getContinentQuery, [continent], (err, continentResult) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (continentResult.length > 0) {
+        // If the continent exists, insert the relationship into Animal_Continents
+        const continentId = continentResult[0].id;
+        const linkAnimalContinentQuery = `INSERT INTO Animal_Continents (animal_id, continent_id) VALUES (?, ?)`;
+        db.query(linkAnimalContinentQuery, [result.insertId, continentId], (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          // Redirect to the homepage after successfully adding the animal
+          res.redirect('/');
+        });
+      } else {
+        // If the continent does not exist, send an error message
+        res.send("Continent does not exist.");
+      }
+    });
+  });
+});
+
+// About route
+app.get('/about', (req, res) => {
+  res.render('about', { name: 'Najma' });  // Pass your name to the about page
+});
+
+// Route for searching animals (GET)
+app.get('/search', (req, res, next) => {
+  const searchText = req.query.search_text || '';  // Get search text from query string
+  const query = `
+    SELECT Animals.name AS animal_name, Animals.species, Continents.continent_name 
+    FROM Animals
+    JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
+    JOIN Continents ON Animal_Continents.continent_id = Continents.id
+    WHERE Animals.name LIKE ? OR Animals.species LIKE ? OR Continents.continent_name LIKE ?`;
+
+  // Use the searchText value in the query
+  db.query(query, [`%${searchText}%`, `%${searchText}%`, `%${searchText}%`], (err, animals) => {
+    if (err) {
+      return next(err);
+    }
+
+    // Render the search page with search results and search text
+    res.render('search', { animals, searchText });
+  });
+});
 
 // Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
--- routes/main.js
+++ routes/main.js
@@ -1,100 +1,106 @@
-const express = require("express")
-const router = express.Router()
-const request = require('request');
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
 
+// Database connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'portfolio_app',
+  password: 'qwertyuiop',
+  database: 'portfolio'
+});
 
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to the database');
+});
+
+// Route for the homepage (GET)
 router.get('/', (req, res, next) => {
-    // SQL query to retrieve all animals
-    const sqlquery = `
-        SELECT Animals.animal_name, Animals.species, Continents.continent_name 
-        FROM Animals
-        JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
-        JOIN Continents ON Animal_Continents.continent_id = Continents.id
-    `;
+  const query = `SELECT Animals.name AS animal_name, Animals.species, Continents.continent_name 
+                 FROM Animals
+                 JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
+                 JOIN Continents ON Animal_Continents.continent_id = Continents.id`;
 
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            return next(err);
-        }
+  db.query(query, (err, animals) => {
+    if (err) {
+      return next(err);
+    }
 
-        // Render the homepage with the list of animals
-        res.render('index', { animals: result });
-    });
+    res.render('index', { animals });
+  });
 });
 
-// Route to handle the addition of a new animal
-router.post('/', (req, res, next) => {
-    const { animal_name, species, continent } = req.body;
-
-    // Insert the new animal into the Animals table
-    const animalQuery = `INSERT INTO Animals (animal_name, species) VALUES (?, ?)`;
-    db.query(animalQuery, [animal_name, species], (err, result) => {
-        if (err) {
-            return next(err);
-        }
-
-        // After inserting the animal, find the corresponding continent
-        const getContinentQuery = `SELECT id FROM Continents WHERE continent_name = ?`;
-        db.query(getContinentQuery, [continent], (err, continentResult) => {
-            if (err) {
-                return next(err);
-            }
-
-            if (continentResult.length > 0) {
-                // If the continent exists, insert the relationship into Animal_Continents
-                const continentId = continentResult[0].id;
-                const linkAnimalContinentQuery = `INSERT INTO Animal_Continents (animal_id, continent_id) VALUES (?, ?)`;
-                db.query(linkAnimalContinentQuery, [result.insertId, continentId], (err) => {
-                    if (err) {
-                        return next(err);
-                    }
-
-                    // Redirect to the homepage after successfully adding the animal
-                    res.redirect('/');
-                });
-            } else {
-                // If the continent does not exist, send an error message
-                res.send("Continent does not exist.");
-            }
-        });
-    });
+// Route for the About page (GET)
+router.get('/about', (req, res) => {
+  res.render('about', { name: 'Najma' });  // Pass your name to the about page
 });
 
-
-
-
-// Route for search functionality
+// Route for searching animals (GET)
 router.get('/search', (req, res, next) => {
-    // Get the search text from the query parameters
-    const searchText = req.query.search_text || ''; 
+  const searchText = req.query.search_text || '';  // Get search text from query string
+  const query = `
+    SELECT Animals.name AS animal_name, Animals.species, Continents.continent_name 
+    FROM Animals
+    JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
+    JOIN Continents ON Animal_Continents.continent_id = Continents.id
+    WHERE Animals.name LIKE ? OR Animals.species LIKE ? OR Continents.continent_name LIKE ?`;
 
-    // Define the SQL query for searching
-    const sqlquery = `
-        SELECT Animals.animal_name, Animals.species, Continents.continent_name 
-        FROM Animals
-        JOIN Animal_Continents ON Animals.id = Animal_Continents.animal_id
-        JOIN Continents ON Animal_Continents.continent_id = Continents.id
-        WHERE Animals.animal_name LIKE ? OR Animals.species LIKE ?
-    `;
+  // Use the searchText value in the query
+  db.query(query, [`%${searchText}%`, `%${searchText}%`, `%${searchText}%`], (err, animals) => {
+    if (err) {
+      return next(err);
+    }
 
-    // Execute the query with wildcard search
-    const searchValue = `%${searchText}%`; 
-    db.query(sqlquery, [searchValue, searchValue], (err, result) => {
-        if (err) {
-            return next(err); 
-        }
+    // Render the search page with search results and search text
+    res.render('search', { animals, searchText });
+  });
+});
 
-        // Render the search page with the results
-        res.render('search', { animals: result, searchText });
+// Route for adding a new animal (POST)
+router.post('/', (req, res, next) => {
+  const { animal_name, species, continent } = req.body;
+
+  // Ensure required fields are provided
+  if (!animal_name || !species || !continent) {
+    return res.status(400).send("Missing required fields.");
+  }
+
+  // Insert the new animal into the Animals table
+  const animalQuery = `INSERT INTO Animals (name, species) VALUES (?, ?)`;
+  db.query(animalQuery, [animal_name, species], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+
+    // After inserting the animal, find the corresponding continent
+    const getContinentQuery = `SELECT id FROM Continents WHERE continent_name = ?`;
+    db.query(getContinentQuery, [continent], (err, continentResult) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (continentResult.length > 0) {
+        // If the continent exists, insert the relationship into Animal_Continents
+        const continentId = continentResult[0].id;
+        const linkAnimalContinentQuery = `INSERT INTO Animal_Continents (animal_id, continent_id) VALUES (?, ?)`;
+        db.query(linkAnimalContinentQuery, [result.insertId, continentId], (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          // Redirect to the homepage after successfully adding the animal
+          res.redirect('/');
+        });
+      } else {
+        // If the continent does not exist, send an error message
+        res.send("Continent does not exist.");
+      }
     });
+  });
 });
 
 module.exports = router;
-
-
-router.get('/about',function(req,res,next){
-    res.render('about.ejs')
-})
-
-
-module.exports = router
