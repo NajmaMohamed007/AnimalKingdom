a53d78877a48ee6aebf7f513296b203ffa2d5f81
najmamohamed007 <nmoha007@campus.goldsmiths.ac.uk>
2025-01-02 19:58:25 +0000

    final3

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
-const mysql = require('mysql');
-const mysql = require('mysql2');
+const mysql = require('mysql2');  // Use only mysql2
 
 // Database connection
 const db = mysql.createConnection({
