26ec347ddda45c416282736348c843adcebeceed
najmamohamed007 <nmoha007@campus.goldsmiths.ac.uk>
2025-01-02 19:52:03 +0000

    final2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8000;
+const mysql = require('mysql');
 const mysql = require('mysql2');
 
 // Database connection
